c70ec7092e097c40d3fbf2476fb29b897f167fb5
Abhijit Kumar Banik <abhijitbanikofficial@gmail.com>
2022-11-30 01:09:55 +0530

    Intial commit

---
+++ src/main/java/com/springboot/service/Dog.java
@@ -0,0 +1,13 @@
+package com.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Dog implements Animal {
+
+	@Override
+	public String characteristics() {
+		// TODO Auto-generated method stub
+		return "Bark";
+	}
+}
